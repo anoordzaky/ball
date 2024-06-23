58ed9f04f7b781a6f739d5557abbb74f28e20df0
Achmadi Noor <achmadinoord@gmail.com>
2024-06-23 14:26:03 +0700

    fixed rgb values on the final image

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def main():
     skybox = Skybox("skybox_3.jpg")
 
     objects = [
-        Ball(np.array([0,0,-10]),2, np.array([0,1,0])),
-        Ball(np.array([-5,0,-10]),2, np.array([.6,.2,.8])),
+        Ball(np.array([5,1,-15]),2, np.array([0,1,0])),
+        Ball(np.array([-6,0,-15]),2, np.array([0,0,1])),
         Floor(2, np.array([0,0,0]), np.array([1,1,1]))
     ]
     
-    light = Light(direction=np.array([8,10,-15]), intensity=1.0)
+    light = Light(direction=np.array([-1,1,-1]), intensity=1.0)
 
     frame = np.empty(shape=[camera.resolution[1], camera.resolution[0], 3], dtype=np.uint8)
 
@@ -34,17 +34,17 @@ def main():
                 normal = object.normal(intersect)
                 color = object.surface_color(intersect)
 
-                light_ray = Ray(intersect + normal*shadow_bias, -light.direction)
+                light_ray = Ray(intersect + normal, -light.direction)
                 light_intersect, _ = light_ray.cast(objects=objects)
                 if type(light_intersect) == np.ndarray:
-                    color = color * .1 / light.intensity
-                color =  color * max(np.dot(normal, -light.direction * light.intensity),0)
+                    color = color * .1 / light.intensity          
+                color =  color * max([np.dot(normal,-light.direction * light.intensity),0])
             else:
                 color = skybox.get_color(camera_ray.direction)
 
             frame[y,x] = color * 255
 
-
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     cv2.imshow("test frame",frame)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
